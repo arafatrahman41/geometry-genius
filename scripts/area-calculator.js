0ad4f7b51cfe23c788b78b0e2c742b3fbdb24bdf
arafatrahman41 <arafatrahman41@outlook.com>
2023-08-20 20:34:07 +0600

    added rectangle area

--- scripts/area-calculator.js
+++ scripts/area-calculator.js
@@ -16,3 +16,25 @@ function calculateTriangleArea(){
  const areaSpan = document.getElementById('triangle-area');
  areaSpan.innerText = area; 
 }
+
+function calculateRectangleArea(){
+  //get rectangle width
+  const widthField = document.getElementById('rectangle-width');
+  const widthValueText = widthField.value;
+  const width = parseFloat(widthValueText);
+  console.log(width);
+
+  //get rectangle length
+   const lengthField = document.getElementById('rectangle-length');
+   const lengthValueText = lengthField.value;
+   const length = parseFloat(lengthValueText);
+   console.log(length);
+
+   //calculate rectangle area
+   const area = width * length;
+   console.log(area);
+
+   //show rectangle area
+   const rectangleAreaSpan = document.getElementById('rectangle-area');
+   rectangleAreaSpan.innerText = area;
+}
